079543138a9a9521c8c8a664c160e5f04f641a22
Charlyn Pepito <charlynpepito50@gmail.com>
2017-09-10 21:04:35 -0700

    udpated recommendation

--- app/src/main/java/cabiso/daphny/com/g_companion/Recommend/Bottle_Recommend.java
+++ app/src/main/java/cabiso/daphny/com/g_companion/Recommend/Bottle_Recommend.java
@@ -1,29 +1,42 @@
 package cabiso.daphny.com.g_companion.Recommend;
 
 import android.app.ProgressDialog;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import cabiso.daphny.com.g_companion.DIYActivity;
 import cabiso.daphny.com.g_companion.R;
+import cabiso.daphny.com.g_companion.UploadDIYAdapter;
 
 public class Bottle_Recommend extends AppCompatActivity {
 
     public TextView name,material, procedure;
-    private List<DIYrecommend> diyList;
     private RecommendDIYAdapter adapter;
-    private ArrayList<String> diys;
+    private ImageView loadview;
+    private FirebaseStorage storage;
+    private ListView lv;
 
     public ProgressDialog progressDialog;
     @Override
@@ -32,47 +45,42 @@ public class Bottle_Recommend extends AppCompatActivity {
         setContentView(R.layout.activity_recommend_bottle);
 
         name = (TextView)findViewById(R.id.tvDiyName);
-//        material = (TextView)findViewById(R.id.tvDiyMaterial);
-//        procedure = (TextView)findViewById(R.id.tvProcedure);
+        material = (TextView)findViewById(R.id.tvDiyMaterial);
+        procedure = (TextView)findViewById(R.id.tvDiyProcedure);
+        loadview = (ImageView)findViewById(R.id.imgView);
+        lv = (ListView) findViewById(R.id.lvView);
 
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Please Wait loading DIYs.....");
         progressDialog.show();
 
-//        databaseReference = FirebaseDatabase.getInstance().getReference("DIY_Methods").child("bottle");
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("DIY_Methods").child("category").child("bottle");
 
+        final List<DIYrecommend> diyList = new ArrayList<>();
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-            String getDiy = "DIYS: ";
-                    diys = new ArrayList<String>();
-
+                diyList.clear();
                 for(DataSnapshot postSnapshot:dataSnapshot.getChildren()){
-                    diys.add(String.valueOf(postSnapshot.getValue()));
-                    Toast.makeText(getApplication(),"GET DIYS: "+diys,Toast.LENGTH_SHORT).show();
-                    Log.e("GET DIYS: "+diys, "KUHAAAAAAa");
-                    for(int i=0;i<diys.size();i++){
-                        getDiy +="\n"+diys.get(i);
-                    }
+                    DIYrecommend recommend  = postSnapshot.getValue(DIYrecommend.class);
+                    Toast.makeText(getApplication(),"GET DIYS: "+diyList,Toast.LENGTH_SHORT).show();
+                    diyList.add(recommend);
+
+                    String nm =  "NAME: "+recommend.getDiyName();
+                    String mat = "MATERIAL: "+recommend.getDiymaterial();
+                    String pro = "PROCEDURE: "+recommend.getDiyprocedure();
+                    String image = recommend.getImage_URL();
+
+                    name.setText(nm);
+                    material.setText(mat);
+                    procedure.setText(pro);
+                    //init adapter
+                    adapter = new RecommendDIYAdapter(Bottle_Recommend.this, R.layout.fragment_ui_items, diyList);
+                    //set adapter for listview
+                    lv.setAdapter(adapter);
                 }
-                name.setText(getDiy);
-
-//                    DIYrecommend recommend = dataSnapshot.getValue(DIYrecommend.class);
-//                    String nm = "Name: "+recommend.getDiyName();
-//                    Log.d("NAME: "+nm," ");
-//                    String mtrial = "Material: "+recommend.getDiymaterial();
-//                    String proc = "Procedure: "+recommend.getDiyprocedure();
-//                    String id = dataSnapshot.getKey();
-//                    Toast.makeText(getApplication(),"ID: "+id,Toast.LENGTH_SHORT).show();
-//
-//                    name.setText(nm);
-//                    material.setText(mtrial);
-//                    procedure.setText(proc);
-
-
-
                 progressDialog.dismiss();
             }
 
@@ -81,24 +89,5 @@ public class Bottle_Recommend extends AppCompatActivity {
                 Log.d("Error: ","The read failed!"+databaseError.getMessage());
             }
         });
-
-//        Query query = databaseReference.child("bottle");
-//        query.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                for (DataSnapshot snapshot:dataSnapshot.getChildren()){
-//                    DIYrecommend img = snapshot.getValue(DIYrecommend.class);
-//                    diyList.add(img);
-//                }
-//                adapter = new RecommendDIYAdapter(Recommend_Activity.this, R.layout.fragment_ui_items, diyList);
-//                //set adapter for listview
-//                lv.setAdapter(adapter);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
     }
 }

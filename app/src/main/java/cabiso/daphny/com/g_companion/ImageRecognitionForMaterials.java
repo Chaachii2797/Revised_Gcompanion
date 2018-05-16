666cbfc2cfb46660e3391b961ee0960e198f6539
Daphny Cabiso <daphnycabiso@bsit.usjr.edu.ph>
2018-05-16 19:28:25 +0800

    all pictures in the listview wll be updated

--- app/src/main/java/cabiso/daphny/com/g_companion/ImageRecognitionForMaterials.java
+++ app/src/main/java/cabiso/daphny/com/g_companion/ImageRecognitionForMaterials.java
@@ -1,14 +1,19 @@
 package cabiso.daphny.com.g_companion;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,19 +23,26 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.cunoraz.tagview.Tag;
+import com.cunoraz.tagview.TagView;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import cabiso.daphny.com.g_companion.Adapter.NewAddedMaterial;
+import cabiso.daphny.com.g_companion.Model.Constants;
 import cabiso.daphny.com.g_companion.Model.DBMaterial;
 import cabiso.daphny.com.g_companion.Model.ImgRecogSetQty;
+import cabiso.daphny.com.g_companion.Model.TagClass;
 import cabiso.daphny.com.g_companion.Recommend.Bottle_Recommend;
 import clarifai2.api.ClarifaiBuilder;
 import clarifai2.api.ClarifaiClient;
@@ -53,10 +65,12 @@ public class ImageRecognitionForMaterials extends AppCompatActivity {
 
     private EditText etAddMaterial;
     private Spinner qtySpinner, umSpinner;
-    private ImageView imageMaterial;
+    private ImageView imageMaterial, addedMat_image;
     private ListView materialsAddedLv;
     private Button btnAddMaterial, btnForDIY;
     private ProgressDialog progressDialog;
+    private TagView tagGroup;
+    private ArrayList<TagClass> tagList;
 
     private List<String> tags = new ArrayList<>();
     private List<String> extras = new ArrayList<>();
@@ -65,6 +79,7 @@ public class ImageRecognitionForMaterials extends AppCompatActivity {
     private ArrayList<ImgRecogSetQty> anotherMaterialSetQties;
     private ArrayList<DBMaterial> dbMaterials;
     private NewAddedMaterial newAddMatAdapter;
+    private static Bitmap bitmap_transfer;
 
     String[] unitOfMeasurement;
     String[] quantity;
@@ -87,6 +102,7 @@ public class ImageRecognitionForMaterials extends AppCompatActivity {
         materialsAddedLv = (ListView) findViewById(R.id.lvMaterialList);
         btnAddMaterial = (Button) findViewById(R.id.btn_add_another_material);
         btnForDIY = (Button) findViewById(R.id.btnForDIY);
+        tagGroup = (TagView) findViewById(R.id.tag_group_imageRecog);
 
         anotherMaterialSetQties  = new ArrayList<>();
         ImgRecogSetQty item1 = new ImgRecogSetQty().setName("test11");
@@ -96,6 +112,59 @@ public class ImageRecognitionForMaterials extends AppCompatActivity {
 
         quantity = getResources().getStringArray(R.array.qty);
         unitOfMeasurement = getResources().getStringArray(R.array.UM);
+
+        etAddMaterial.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                setTags(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        tagGroup.setOnTagLongClickListener(new TagView.OnTagLongClickListener() {
+            @Override
+            public void onTagLongClick(Tag tag, int position) {
+                Toast.makeText(ImageRecognitionForMaterials.this, "Long Click: " + tag.text, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tagGroup.setOnTagClickListener(new TagView.OnTagClickListener() {
+            @Override
+            public void onTagClick(Tag tag, int position) {
+                etAddMaterial.setText(tag.text);
+                etAddMaterial.setSelection(tag.text.length());//to set cursor position
+
+            }
+        });
+        tagGroup.setOnTagDeleteListener(new TagView.OnTagDeleteListener() {
+
+            @Override
+            public void onTagDeleted(final TagView view, final Tag tag, final int position) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ImageRecognitionForMaterials.this, R.style.AppTheme);
+                builder.setMessage("\"" + tag.text + "\" will be delete. Are you sure?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        view.remove(position);
+                        Toast.makeText(ImageRecognitionForMaterials.this, "\"" + tag.text + "\" deleted", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("No", null);
+                builder.show();
+
+            }
+        });
+
+        prepareTags();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.imToolbar);
         setSupportActionBar(toolbar);
@@ -124,41 +193,41 @@ public class ImageRecognitionForMaterials extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 dispatchTakePictureIntent();
-                //printTags();
                 etAddMaterial.setEnabled(true);
                 qtySpinner.setEnabled(true);
                 umSpinner.setEnabled(true);
-
             }
+
         });
 
         etAddMaterial.setEnabled(false);
         qtySpinner.setEnabled(false);
         umSpinner.setEnabled(false);
 
+
         btnAddMaterial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String added_tag = etAddMaterial.getText().toString();
+                String added_tag = etAddMaterial.getText().toString().toLowerCase();
                 String added_qty = qtySpinner.getSelectedItem().toString();
                 String added_um = umSpinner.getSelectedItem().toString();
+                String mat_image = imageMaterial.getDrawable().toString();
 
                 if(!added_tag.isEmpty()){
-//                    dbMaterials.add(new DBMaterial().setName(added_qty + " " + added_um + " " + added_tag));
-                    dbMaterials.add(new DBMaterial().setName(added_tag).setUnit(added_um).setQuantity(Integer.parseInt(added_qty)));
+                    dbMaterials.add(new DBMaterial().setName(added_tag).setUnit(added_um)
+                            .setQuantity(Integer.parseInt(added_qty)));
                     newAddMatAdapter.notifyDataSetChanged();
 
                     etAddMaterial.setText("");
                     qtySpinner.setSelection(0);
                     umSpinner.setSelection(0);
+
+                    imageMaterial.setImageResource(R.drawable.add);
+
                 }else{
                     Toast.makeText(ImageRecognitionForMaterials.this, "Add material cannot be empty!", Toast.LENGTH_SHORT).show();
                 }
-
-//                    qtySpinner.setAdapter(null);
-//                    umSpinner.setAdapter(null);
-//
             }
         });
 
@@ -189,6 +258,7 @@ public class ImageRecognitionForMaterials extends AppCompatActivity {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+
         }
     }
 
@@ -261,7 +331,33 @@ public class ImageRecognitionForMaterials extends AppCompatActivity {
                 // convert byte array to Bitmap
                 Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0,
                         byteArray.length);
+
                 imageMaterial.setImageBitmap(bitmap);
+
+                //Ika pic ma update tanan naa sa listview sht
+//                imageMaterial.buildDrawingCache();
+//                setBitmap_transfer(imageMaterial.getDrawingCache());
+
+                constan.photoMap = bitmap;
+
+
+
+                //Ma pass deretso ang image, dli pa siya ma set sa image view
+//                Intent in = new Intent(this,ImageRecognitionForMaterials.class);
+//                Drawable drbl= imageMaterial.getDrawable();
+//                Bitmap bit = ((BitmapDrawable)drbl).getBitmap();
+//                in.putExtra("Bitmap",bit);
+//                Log.e("Bitmap", bit.toString());
+//                startActivity(in);
+
+
+
+                // CALL THIS METHOD TO GET THE URI FROM THE BITMAP
+//                Uri tempUri = getImageUri(getApplicationContext(), bmp);
+//                Log.e("mat_URL", tempUri.toString());
+
+                Toast.makeText(this, " " + bmp.toString() , Toast.LENGTH_SHORT).show();
+                Log.e("mat_URL", bitmap.toString());
 
                 new AsyncTask<Bitmap, Void, ClarifaiResponse<List<ClarifaiOutput<Concept>>>>() {
                     // Model prediction
@@ -278,12 +374,15 @@ public class ImageRecognitionForMaterials extends AppCompatActivity {
 //                        ClarifaiResponse<List<Concept>> card = client.searchConcepts(String.valueOf(SearchClause.matchConcept(Concept
 //                                .forName("cardboard")))).getPage(0).executeSync();
 
-                        return generalModel.predict().withInputs(ClarifaiInput.forImage(ClarifaiImage.of(byteArray))).executeSync();
+                        return generalModel.predict().withInputs(ClarifaiInput.forImage(ClarifaiImage.of(byteArray)))
+                                .executeSync();
+
 //                        return client.getDefaultModels().generalModel().predict()
 //                                .withInputs(ClarifaiInput.forImage(ClarifaiImage.of(byteArray)))
 //                                .executeSync();
 
                     }
+
 
                     // Handling API response and then collecting and printing tags
                     @Override
@@ -306,14 +405,16 @@ public class ImageRecognitionForMaterials extends AppCompatActivity {
 //                                ImgRecogSetQty imgRecogSetQty = new ImgRecogSetQty();
 //                                imgRecogSetQty.setName(predictedTags.get(i).name());
 //                                anotherMaterialSetQties.add(imgRecogSetQty);
-
                             }
-                            printTags();
+                            //printTags();
+
                         }catch (ClarifaiException ex){
                             ex.getMessage();
                         }
                     }
                 }.execute(bitmap);
+
+
             }
         } else if (resultCode == RESULT_CANCELED) {
             // User cancelled the image capture or selection.
@@ -323,4 +424,70 @@ public class ImageRecognitionForMaterials extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Unknown Failure. Please notify app owner.", Toast.LENGTH_SHORT).show();
         }
     }
+//    public Uri getImageUri(Context inContext, Bitmap inImage) {
+//        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+//        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+//        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+//        return Uri.parse(path);
+//    }
+
+    //Materials Tags
+    private void prepareTags() {
+        tagList = new ArrayList<>();
+        JSONArray jsonArray;
+        JSONObject temp;
+        try {
+            jsonArray = new JSONArray(Constants.MATERIALS);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                temp = jsonArray.getJSONObject(i);
+                tagList.add(new TagClass(temp.getString("code"), temp.getString("name")));
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void setTags(CharSequence cs) {
+        /**
+         * for empty edittext
+         */
+        if (cs.toString().equals("")) {
+            tagGroup.addTags(new ArrayList<Tag>());
+            return;
+        }
+
+        String text = cs.toString();
+        ArrayList<Tag> tags = new ArrayList<>();
+        Tag tag;
+
+        for (int i = 0; i < tagList.size(); i++) {
+            if (tagList.get(i).getName().toLowerCase().startsWith(text.toLowerCase())) {
+                tag = new Tag(tagList.get(i).getName());
+                tag.radius = 10f;
+                tag.layoutColor = Color.parseColor(tagList.get(i).getColor());
+                if (i % 2 == 0) // you can set deletable or not
+                    tag.isDeletable = true;
+                tags.add(tag);
+            }
+        }
+        tagGroup.addTags(tags);
+
+    }
+
+
+
+//    public static Bitmap getBitmap_transfer() {
+//        return bitmap_transfer;
+//    }
+//
+//    public static void setBitmap_transfer(Bitmap bitmap_transfer_param) {
+//        bitmap_transfer = bitmap_transfer_param;
+//    }
+
+    public static class constan {
+        public static Bitmap photoMap = null;
+    }
+
 }
--- app/src/main/java/cabiso/daphny/com/g_companion/Model/Constants.java
+++ app/src/main/java/cabiso/daphny/com/g_companion/Model/Constants.java
@@ -7,6 +7,7 @@ package cabiso.daphny.com.g_companion.Model;
 public class Constants {
 
     public static final String MATERIALS = "[ \n" +
+            "{\"name\": \"Acrylic Paint\", \"code\": \"AP\"}, \n" +
             "{\"name\": \"Aluminum\", \"code\": \"AL\"}, \n" +
             "{\"name\": \"Aerosol Cans\", \"code\": \"AE\"}, \n" +
             "{\"name\": \"Aluminium Foil\", \"code\": \"AF\"}, \n" +
@@ -15,10 +16,13 @@ public class Constants {
             "{\"name\": \"Beads\", \"code\": \"BE\"}, \n" +
             "{\"name\": \"Book\", \"code\": \"BO\"}, \n" +
             "{\"name\": \"Bottle\", \"code\": \"BT\"}, \n" +
+            "{\"name\": \"Bottle Cap\", \"code\": \"BC\"}, \n" +
+            "{\"name\": \"Bottle Pourer\", \"code\": \"BP\"}, \n" +
             "{\"name\": \"Box\", \"code\": \"BX\"}, \n" +
             "{\"name\": \"Broom\", \"code\": \"BR\"}, \n" +
             "{\"name\": \"Buttons\", \"code\": \"BU\"}, \n" +
             "{\"name\": \"Cans\", \"code\": \"CA\"}, \n" +
+            "{\"name\": \"Canvas\", \"code\": \"CA\"}, \n" +
             "{\"name\": \"Caps\", \"code\": \"CP\"}, \n" +
             "{\"name\": \"Card\", \"code\": \"CR\"}, \n" +
             "{\"name\": \"Cardboard\", \"code\": \"CB\"}, \n" +
@@ -38,18 +42,24 @@ public class Constants {
             "{\"name\": \"Crayons\", \"code\": \"CY\"}, \n" +
             "{\"name\": \"Cube\", \"code\": \"CU\"}, \n" +
             "{\"name\": \"Cup\", \"code\": \"KU\"}, \n" +
+            "{\"name\": \"Cutter\", \"code\": \"CT\"}, \n" +
             "{\"name\": \"Coins\", \"code\": \"CS\"}, \n" +
             "{\"name\": \"Disc\", \"code\": \"DI\"}, \n" +
             "{\"name\": \"Dustpan\", \"code\": \"DU\"}, \n" +
             "{\"name\": \"Envelope\", \"code\": \"EN\"}, \n" +
             "{\"name\": \"Egg Shells\", \"code\": \"EG\"}, \n" +
             "{\"name\": \"Fan\", \"code\": \"FA\"}, \n" +
+            "{\"name\": \"Fastener\", \"code\": \"FS\"}, \n" +
             "{\"name\": \"Foam\", \"code\": \"FO\"}, \n" +
             "{\"name\": \"Foil\", \"code\": \"FI\"}, \n" +
             "{\"name\": \"Fork\", \"code\": \"FR\"}, \n" +
             "{\"name\": \"Frame\", \"code\": \"FR\"}, \n" +
             "{\"name\": \"Furniture\", \"code\": \"FU\"}, \n" +
             "{\"name\": \"Glass\", \"code\": \"GL\"}, \n" +
+            "{\"name\": \"Glitter Glue\", \"code\": \"GU\"}, \n" +
+            "{\"name\": \"Glitters\", \"code\": \"GI\"}, \n" +
+            "{\"name\": \"Glue\", \"code\": \"GL\"}, \n" +
+            "{\"name\": \"Glue Gun\", \"code\": \"GG\"}, \n" +
             "{\"name\": \"Hangers\", \"code\": \"HA\"}, \n" +
             "{\"name\": \"Irons\", \"code\": \"IR\"}, \n" +
             "{\"name\": \"Ink/Toner Cartridges\", \"code\": \"IN\"}, \n" +
@@ -57,6 +67,7 @@ public class Constants {
             "{\"name\": \"Jewel\", \"code\": \"JE\"}, \n" +
             "{\"name\": \"Jugs\", \"code\": \"JU\"}, \n" +
             "{\"name\": \"Key\", \"code\": \"KE\"}, \n" +
+            "{\"name\": \"Knife\", \"code\": \"KN\"}, \n" +
             "{\"name\": \"Laptop\", \"code\": \"LA\"}, \n" +
             "{\"name\": \"Lightbulbs\", \"code\": \"LI\"}, \n" +
             "{\"name\": \"Leaves\", \"code\": \"LE\"}, \n" +
@@ -72,6 +83,7 @@ public class Constants {
             "{\"name\": \"Oil\", \"code\": \"OI\"}, \n" +
             "{\"name\": \"Paintbrush\", \"code\": \"PA\"}, \n" +
             "{\"name\": \"Paper\", \"code\": \"PP\"}, \n" +
+            "{\"name\": \"Paper Plate\", \"code\": \"PP\"}, \n" +
             "{\"name\": \"Pen\", \"code\": \"PE\"}, \n" +
             "{\"name\": \"Pencil\", \"code\": \"PN\"}, \n" +
             "{\"name\": \"Phone\", \"code\": \"PH\"}, \n" +
@@ -80,17 +92,20 @@ public class Constants {
             "{\"name\": \"Plastic Bottle\", \"code\": \"PS\"}, \n" +
             "{\"name\": \"Plates\", \"code\": \"PL\"}, \n" +
             "{\"name\": \"Paint\", \"code\": \"PA\"}, \n" +
+            "{\"name\": \"Paint pen\", \"code\": \"PE\"}, \n" +
             "{\"name\": \"Plastics\", \"code\": \"PL\"}, \n" +
             "{\"name\": \"Pizza Box\", \"code\": \"PZ\"}, \n" +
             "{\"name\": \"Plastic Bags\", \"code\": \"PC\"}, \n" +
             "{\"name\": \"Pottery\", \"code\": \"PO\"}, \n" +
             "{\"name\": \"Rubber\", \"code\": \"RU\"}, \n" +
             "{\"name\": \"Rug\", \"code\": \"RG\"}, \n" +
+            "{\"name\": \"Sandpaper\", \"code\": \"SN\"}, \n" +
             "{\"name\": \"Scatch Tape\", \"code\": \"SA\"}, \n" +
             "{\"name\": \"Scissors\", \"code\": \"SC\"}, \n" +
             "{\"name\": \"Shoe\", \"code\": \"SH\"}, \n" +
             "{\"name\": \"Sling\", \"code\": \"SL\"}, \n" +
             "{\"name\": \"Spoon\", \"code\": \"SP\"}, \n" +
+            "{\"name\": \"Spray\", \"code\": \"SR\"}, \n" +
             "{\"name\": \"Steel\", \"code\": \"ST\"}, \n" +
             "{\"name\": \"Stick\", \"code\": \"SI\"}, \n" +
             "{\"name\": \"Straw\", \"code\": \"SW\"}, \n" +
@@ -109,10 +124,15 @@ public class Constants {
             "{\"name\": \"Video Cassettes\", \"code\": \"VI\"}, \n" +
             "{\"name\": \"Watch\", \"code\": \"WA\"}, \n" +
             "{\"name\": \"Wine\", \"code\": \"WI\"}, \n" +
+            "{\"name\": \"Wine Bottle\", \"code\": \"WB\"}, \n" +
+            "{\"name\": \"Wine Cork\", \"code\": \"WC\"}, \n" +
             "{\"name\": \"Wine Glass\", \"code\": \"WN\"}, \n" +
             "{\"name\": \"Wood\", \"code\": \"WO\"}, \n" +
+            "{\"name\": \"Zipper\", \"code\": \"ZI\"}, \n" +
+            "{\"name\": \"Wood Frame\", \"code\": \"WF\"}, \n" +
             "{\"name\": \"Wrapping Paper\", \"code\": \"WR\"}, \n" +
             "{\"name\": \"Yoghurt Pots\", \"code\": \"YO\"} \n" +
+
 
             "]";
 

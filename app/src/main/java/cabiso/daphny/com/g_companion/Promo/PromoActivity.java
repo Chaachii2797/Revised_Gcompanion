c5077f26fb8470cbd330aa44afd7d6b757d003a2
Charlyn Pepito <charlynpepito50@gmail.com>
2018-08-10 20:21:09 +0800

    revising the final ui promo -tiwason ugma for db and viewing

--- app/src/main/java/cabiso/daphny/com/g_companion/Promo/PromoActivity.java
+++ app/src/main/java/cabiso/daphny/com/g_companion/Promo/PromoActivity.java
@@ -1,14 +1,23 @@
 package cabiso.daphny.com.g_companion.Promo;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -19,20 +28,25 @@ import com.google.firebase.database.FirebaseDatabase;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
+import cabiso.daphny.com.g_companion.DIYDetailViewActivity;
 import cabiso.daphny.com.g_companion.Model.DIYnames;
 import cabiso.daphny.com.g_companion.Model.SellingDIY;
 import cabiso.daphny.com.g_companion.R;
 
-public class PromoActivity extends AppCompatActivity {
+public class PromoActivity extends AppCompatActivity implements View.OnClickListener {
     private DatabaseReference promoReference;
+    private DatabaseReference promoAddReference;
 
     private TextView mTvProductName;
     private TextView mTvProductDesc;
     private TextView mTvFinish;
     private ListView mListviewPromo;
-    private Button mBtnExpiry;
+    private TextView mTvExpiry;
+    private ImageButton mIbBack;
+    private ArrayList<DIYnames> mListSelectedItems;
 
     private PromoAdapterForList promo_adapter;
     private ArrayList<DIYnames> promoList;
@@ -45,17 +59,40 @@ public class PromoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_promo);
 
         promoList = new ArrayList<>();
+        mListSelectedItems = new ArrayList<>();
 
         mTvProductName = (TextView) findViewById(R.id.tvProductName);
-        mTvProductDesc = (TextView) findViewById(R.id.tvProductDesc);
         mListviewPromo = (ListView) findViewById(R.id.lvPromoItemList);
         mTvFinish = (TextView) findViewById(R.id.tv_finish);
-        mBtnExpiry = (Button) findViewById(R.id.btn_set_expiry_date);
+        mTvExpiry = (TextView) findViewById(R.id.tv_set_expiry_date);
+        mIbBack = (ImageButton) findViewById(R.id.ibBlack);
+//        mBtntForCheck = (ImageView) findViewById(R.id.img_for_checked);
 
         Intent intent = getIntent();
         itemId = intent.getExtras().getString("diy_Name");
         mTvProductName.setText(itemId);
 
+        mTvExpiry.setOnClickListener(this);
+        mIbBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back = new Intent(PromoActivity.this, DIYDetailViewActivity.class);
+                startActivity(back);
+            }
+        });
+
+        mTvFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(PromoActivity.this,"CLICKED!",Toast.LENGTH_SHORT);
+                Log.e("LETSE",mListSelectedItems.size()+"");
+            }
+        });
+
+        promo_adapter = new PromoAdapterForList(PromoActivity.this, R.layout.promo_item, promoList,mListSelectedItems);
+        mListviewPromo.setAdapter(promo_adapter);
+
+        promoAddReference = FirebaseDatabase.getInstance().getReference().child("diy_by_tags").child(itemId).getRef();
         promoReference = FirebaseDatabase.getInstance().getReference().child("diy_by_tags");
         promoReference.addChildEventListener(new ChildEventListener() {
             @Override
@@ -65,9 +102,7 @@ public class PromoActivity extends AppCompatActivity {
 
                 if(itemId.equals(diYnames.getDiyName())){
                     promoList.add(diYnames);
-                    promo_adapter = new PromoAdapterForList(PromoActivity.this, R.layout.promo_item, promoList);
-                    mListviewPromo.setAdapter(promo_adapter);
-                    mTvProductDesc.setText(sellingDIY.getSelling_descr());
+                    promo_adapter.notifyDataSetChanged();
                 }
             }
 
@@ -91,7 +126,32 @@ public class PromoActivity extends AppCompatActivity {
 
             }
         });
+    }
 
 
+
+    @Override
+    public void onClick(View v) {
+        if(v==mTvExpiry){
+            final Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int date = calendar.get(Calendar.DAY_OF_MONTH);
+
+            final DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    String set_expiry = year + "-" + String.valueOf(month + 1) + "-" + (dayOfMonth);
+//                    datePickerDialog.getDatePicker().setMaxDate(calendar.getTimeInMillis());
+//                    String set_expiry = String.valueOf(month + 1) + "/" + (dayOfMonth) + "/" + year;
+                    if (calendar.before(set_expiry)) {
+                        Toast.makeText(getApplication(), "YOU CANNOT PICK PASSED WEEKS!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        mTvExpiry.setText(set_expiry);
+                    }
+                }
+            }, year, month, date);
+            datePickerDialog.show();
+        }
     }
 }
--- app/src/main/java/cabiso/daphny/com/g_companion/Promo/PromoAdapterForList.java
+++ app/src/main/java/cabiso/daphny/com/g_companion/Promo/PromoAdapterForList.java
@@ -9,11 +9,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,17 +33,19 @@ public class PromoAdapterForList extends ArrayAdapter<DIYnames>{
     private int resource;
     List<DIYnames> promo_list;
     ArrayList<SellingDIY> selling_list;
+    ArrayList<DIYnames> mListSelectedItems;
 
-    public PromoAdapterForList(@NonNull Activity context, @LayoutRes int resource, @NonNull List<DIYnames> promo) {
+    public PromoAdapterForList(@NonNull Activity context, @LayoutRes int resource, @NonNull List<DIYnames> promo, ArrayList<DIYnames> listSelectedItems) {
         super(context, resource, promo);
         this.context = context;
         this.resource = resource;
+        this.mListSelectedItems = listSelectedItems;
         promo_list = promo;
     }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         final LayoutInflater inflater = context.getLayoutInflater();
 
         View promo_view = inflater.inflate(resource, null);
@@ -48,6 +53,21 @@ public class PromoAdapterForList extends ArrayAdapter<DIYnames>{
         TextView promo_name = (TextView) promo_view.findViewById(R.id.promo_item_name);
         TextView promo_price = (TextView) promo_view.findViewById(R.id.promo_price);
         TextView promo_identity_name = (TextView) promo_view.findViewById(R.id.promo_identity);
+//        ImageView promo_for_checked = (ImageView) promo_view.findViewById(R.id.img_for_checked);
+//        promo_for_checked.setVisibility(View.INVISIBLE);
+        final CheckBox promo_check = (CheckBox) promo_view.findViewById(R.id.cb_for_promo_checked);
+
+        promo_check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                if(promo_check.isChecked()){
+                    mListSelectedItems.add(promo_list.get(position));
+                }else{
+                    mListSelectedItems.remove(promo_list.get(position));
+                }
+            }
+        });
 
         if(promo_list.get(position).getIdentity().equals("selling")){
             promo_identity_name.setText(promo_list.get(position).getIdentity());
